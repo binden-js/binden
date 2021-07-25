949810618e9f96b178376a8604d9c4e146d0a701
Sergey Bakulin <sbakulin@b2broker.net>
2021-07-25 18:44:05 +0300

    feat: add `KauaiError`

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export * from "./src/headers/content-range.js";
 export * from "./src/headers/cookie.js";
 export * from "./src/headers/forwarded.js";
 export * from "./src/headers/range.js";
+export * from "./src/error.js";
 export * from "./src/request.js";
