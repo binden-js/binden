6da48419345350a6fe16241a9f387d4290059852
Sergey Bakulin <sbakulin@b2broker.net>
2021-07-26 14:04:07 +0300

    test: add If-Modified-Since

---
+++ test/headers/if-modified-since.spec.ts
@@ -0,0 +1,28 @@
+import { deepStrictEqual, ok } from "assert";
+
+import { IfModifiedSince } from "../../index.js";
+
+suite("If-Modified-Since", () => {
+  test("constructor", () => {
+    const date = new Date();
+    const ac = new IfModifiedSince({ date });
+    deepStrictEqual(ac.date, date);
+    deepStrictEqual(ac.toString(), date.toUTCString());
+  });
+
+  test("constructor (invalid Date)", () => {
+    const input = "invalid";
+    const date = new Date(input);
+    const ac = new IfModifiedSince({ date });
+    deepStrictEqual(ac.date.getTime(), NaN);
+    deepStrictEqual(ac.toString(), "Invalid Date");
+  });
+
+  test("IfModifiedSince.fromString()", () => {
+    const input = "    Wed, 21 Oct 2015 07:28:00 GMT   ";
+    const parsed = IfModifiedSince.fromString(input);
+    ok(parsed);
+    deepStrictEqual(parsed.date, new Date(input.trim()));
+    deepStrictEqual(parsed.toString(), new Date(input.trim()).toUTCString());
+  });
+});
