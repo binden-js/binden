d1fccc29d8716b178721dfab93108b93c880f8fa
Sergey Bakulin <sergey.bakulin.github@gmail.com>
2022-09-19 10:35:47 +0000

    test: remove redundant types

--- test/context.test.ts
+++ test/context.test.ts
@@ -8,14 +8,13 @@ import {
   BindenError,
   BindenRequest,
   BindenResponse,
-  IBindenResponse,
 } from "../index.js";
 
 const port = 8080;
 const url = `http://localhost:${port}`;
 
 suite("Context", () => {
-  let server: Server;
+  let server: Server<typeof BindenRequest, typeof BindenResponse>;
 
   setup((done) => {
     server = createServer({
@@ -26,30 +25,27 @@ suite("Context", () => {
 
   test("constructor", async () => {
     const serverPromise = new Promise<void>((resolve, reject) => {
-      server.once(
-        "request",
-        (request: BindenRequest, response: IBindenResponse) => {
-          try {
-            const context = new Context({ request, response });
-            ok(context instanceof Context);
-            ok(!context.done);
-            deepStrictEqual(context.request, request);
-            deepStrictEqual(context.response, response);
-            deepStrictEqual(typeof context.id, "string");
-            deepStrictEqual(context.url, new URL(url));
+      server.once("request", (request, response) => {
+        try {
+          const context = new Context({ request, response });
+          ok(context instanceof Context);
+          ok(!context.done);
+          deepStrictEqual(context.request, request);
+          deepStrictEqual(context.response, response);
+          deepStrictEqual(typeof context.id, "string");
+          deepStrictEqual(context.url, new URL(url));
 
-            context.done = true;
-            ok(context.done);
+          context.done = true;
+          ok(context.done);
 
-            context.done = false;
-            ok(context.done);
-          } catch (error) {
-            reject(error);
-          } finally {
-            response.end(resolve);
-          }
+          context.done = false;
+          ok(context.done);
+        } catch (error) {
+          reject(error);
+        } finally {
+          response.end(resolve);
         }
-      );
+      });
     });
     await fetch(url);
     await serverPromise;
@@ -57,28 +53,25 @@ suite("Context", () => {
 
   test(".setHeader()", async () => {
     const serverPromise = new Promise<void>((resolve, reject) => {
-      server.once(
-        "request",
-        (request: BindenRequest, response: IBindenResponse) => {
-          try {
-            const context = new Context({ request, response });
-            const name = "name";
-            const value = ["value1", "value2"];
-            const mock = sinon
-              .mock(response)
-              .expects("setHeader")
-              .once()
-              .withExactArgs(name, value);
+      server.once("request", (request, response) => {
+        try {
+          const context = new Context({ request, response });
+          const name = "name";
+          const value = ["value1", "value2"];
+          const mock = sinon
+            .mock(response)
+            .expects("setHeader")
+            .once()
+            .withExactArgs(name, value);
 
-            deepStrictEqual(context.setHeader(name, value), context);
-            mock.verify();
-          } catch (error) {
-            reject(error);
-          } finally {
-            response.end(resolve);
-          }
+          deepStrictEqual(context.setHeader(name, value), context);
+          mock.verify();
+        } catch (error) {
+          reject(error);
+        } finally {
+          response.end(resolve);
         }
-      );
+      });
     });
     await fetch(url);
     await serverPromise;
@@ -86,28 +79,25 @@ suite("Context", () => {
 
   test("status", async () => {
     const serverPromise = new Promise<void>((resolve, reject) => {
-      server.once(
-        "request",
-        (request: BindenRequest, response: IBindenResponse) => {
-          try {
-            const status = 401;
-            const context = new Context({ request, response });
+      server.once("request", (request, response) => {
+        try {
+          const status = 401;
+          const context = new Context({ request, response });
 
-            const mock = sinon
-              .mock(response)
-              .expects("status")
-              .once()
-              .withExactArgs(status);
+          const mock = sinon
+            .mock(response)
+            .expects("status")
+            .once()
+            .withExactArgs(status);
 
-            deepStrictEqual(context.status(status), context);
-            mock.verify();
-          } catch (error) {
-            reject(error);
-          } finally {
-            response.end(resolve);
-          }
+          deepStrictEqual(context.status(status), context);
+          mock.verify();
+        } catch (error) {
+          reject(error);
+        } finally {
+          response.end(resolve);
         }
-      );
+      });
     });
     await fetch(url);
     await serverPromise;
@@ -126,54 +116,51 @@ suite("Context", () => {
     // eslint-disable-next-line @typescript-eslint/no-loop-func
     test(`.${method}()`, async () => {
       const serverPromise = new Promise<void>((resolve, reject) => {
-        server.once(
-          "request",
-          (request: BindenRequest, response: IBindenResponse) => {
-            const context = new Context({ request, response });
+        server.once("request", (request, response) => {
+          const context = new Context({ request, response });
 
-            const mock = sinon
-              .mock(response)
-              .expects(method)
-              .once()
-              .withExactArgs(args);
+          const mock = sinon
+            .mock(response)
+            .expects(method)
+            .once()
+            .withExactArgs(args);
 
-            try {
-              let promise: Promise<void>;
+          try {
+            let promise: Promise<void>;
 
-              if (method === "send") {
-                deepStrictEqual(args, params[0][1]);
-                promise = context.send(args);
-              } else if (method === "sendFile") {
-                deepStrictEqual(args, params[1][1]);
-                promise = context.sendFile(args);
-              } else if (method === "form") {
-                deepStrictEqual(args, params[2][1]);
-                promise = context.form(args);
-              } else if (method === "html") {
-                deepStrictEqual(args, params[3][1]);
-                promise = context.html(args);
-              } else if (method === "json") {
-                deepStrictEqual(args, params[4][1]);
-                promise = context.json(args);
-              } else {
-                deepStrictEqual(args, params[5][1]);
-                promise = context.text(args);
-              }
-
-              promise
-                .then(() => {
-                  mock.verify();
-                  deepStrictEqual(context.done, true);
-                })
-                .catch(reject)
-                .finally(() => context.response.end(resolve));
-            } catch (error) {
-              context.response.end(() => {
-                reject(error);
-              });
+            if (method === "send") {
+              deepStrictEqual(args, params[0][1]);
+              promise = context.send(args);
+            } else if (method === "sendFile") {
+              deepStrictEqual(args, params[1][1]);
+              promise = context.sendFile(args);
+            } else if (method === "form") {
+              deepStrictEqual(args, params[2][1]);
+              promise = context.form(args);
+            } else if (method === "html") {
+              deepStrictEqual(args, params[3][1]);
+              promise = context.html(args);
+            } else if (method === "json") {
+              deepStrictEqual(args, params[4][1]);
+              promise = context.json(args);
+            } else {
+              deepStrictEqual(args, params[5][1]);
+              promise = context.text(args);
             }
+
+            promise
+              .then(() => {
+                mock.verify();
+                deepStrictEqual(context.done, true);
+              })
+              .catch(reject)
+              .finally(() => context.response.end(resolve));
+          } catch (error) {
+            context.response.end(() => {
+              reject(error);
+            });
           }
-        );
+        });
       });
       await fetch(url);
       await serverPromise;
@@ -182,32 +169,29 @@ suite("Context", () => {
 
   test(".throw()", async () => {
     const serverPromise = new Promise<void>((resolve, reject) => {
-      server.once(
-        "request",
-        (request: BindenRequest, response: IBindenResponse) => {
-          const status = 401;
-          const message = "Text";
-          const expose = true;
-          const context = new Context({ request, response });
-          const json = { message };
+      server.once("request", (request, response) => {
+        const status = 401;
+        const message = "Text";
+        const expose = true;
+        const context = new Context({ request, response });
+        const json = { message };
+        try {
+          context.throw(status, { message, expose, json });
+          reject(new Error("Should throw `BindenError`"));
+        } catch (error: unknown) {
           try {
-            context.throw(status, { message, expose, json });
-            reject(new Error("Should throw `BindenError`"));
-          } catch (error: unknown) {
-            try {
-              ok(error instanceof BindenError);
-              deepStrictEqual(error.expose, expose);
-              deepStrictEqual(error.message, message);
-              deepStrictEqual(error.json, json);
-              deepStrictEqual(error.status, status);
-            } catch (err) {
-              reject(err);
-            }
-          } finally {
-            response.end(resolve);
+            ok(error instanceof BindenError);
+            deepStrictEqual(error.expose, expose);
+            deepStrictEqual(error.message, message);
+            deepStrictEqual(error.json, json);
+            deepStrictEqual(error.status, status);
+          } catch (err) {
+            reject(err);
           }
+        } finally {
+          response.end(resolve);
         }
-      );
+      });
     });
     await fetch(url);
     await serverPromise;
